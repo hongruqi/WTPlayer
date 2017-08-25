588e7aa896be7027dc3d1011ff3167d46069bbe6
hongru qi <hongru.qi@quvideo.com>
2017-08-25 20:09:13 +0800

    Initial commit

---
+++ Example/WTPlayer/WTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WTAppDelegate.h
+//  WTPlayer
+//
+//  Created by lbrsilva-allin on 08/25/2017.
+//  Copyright (c) 2017 lbrsilva-allin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
